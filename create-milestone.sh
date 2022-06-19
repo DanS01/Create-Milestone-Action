c5627165fa35cab5d67b849183cc65573506558c
Danijel Simic <danijel.simic@synergy.net.au>
2022-06-19 19:42:58 +0800

    Full implementation with debugs

--- create-milestone.sh
+++ create-milestone.sh
@@ -2,10 +2,39 @@
 
 # Initialize constants
 LOWER_BOUND=3
+UPPER_BOUND=28
 NEW_TARGET=14
 
 # Initialize variables
 MILESTONE_LINKED=''
+
+# --------------------------------------------------------------------------------
+
+function create_milestone_curl_cmd()
+{
+    # First argument should be set as the milestone title
+    TITLE=${1}
+
+    # Second argument shold be set as the milestone due on date
+    DUE_ON=${2}
+
+    # Need to authenticate to obtain write access for the REST API POST event
+    CREATED_MILESTONE=$( curl --silent -X POST -H "Authorization: token ${SECRETS_TOKEN}" "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"${REPOSITORY}"/milestones -d '{"title":"${TITLE}","due_on":"${DUE_ON}","state":"open"}' )
+}
+
+function link_milestone_curl_cmd()
+{
+    # First argument should be set as the milestone number
+    MILESTONE_NUMBER=${1}
+
+    # The issue or pull request number used to link the milestone to
+    ISSUE_NUMBER="${PULL_REQUEST_NUMBER}"
+
+    # Need to authenticate to obtain write access for the REST API PATCH event
+    LINKED_PULL_REQUEST=$( curl --silent -X PATCH -H "Authorization: token ${SECRETS_TOKEN}" "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"${REPOSITORY}"/issues/"${ISSUE_NUMBER}" -d '{"milestone":"${MILESTONE_NUMBER}"}' )
+}
+
+# --------------------------------------------------------------------------------
 
 # Display information from Github
 echo Github Repository: "${REPOSITORY}"
@@ -17,41 +46,64 @@ echo Pull Request Milestone: "${PULL_REQUEST_MILESTONE}"
 # If there is no milestone linked to the pull request then a value of "null" is returned from the API
 if [[ $PULL_REQUEST_MILESTONE != "null" ]];
 then
-    echo A milestone is already linked
     MILESTONE_LINKED='true'
 else
-    echo There is no milestone linked to the pull request
     MILESTONE_LINKED='false'
 fi
 
 echo Milestone Linked: "$MILESTONE_LINKED"
 
-CURRENT_DATETIME=$( echo $(date +'%Y-%m-%dT%H:%M:%SZ') )
-echo Current DateTime: "$CURRENT_DATETIME"
-LOWER_BOUND_DATETIME=$( echo $(date +'%Y-%m-%dT%H:%M:%SZ' --date "+$LOWER_BOUND days") )
-echo Lower Bound DateTime: "$LOWER_BOUND_DATETIME"
-NEW_TARGET_DATETIME=$( echo $(date +'%Y-%m-%dT%H:%M:%SZ' --date "+$NEW_TARGET days") )
-echo New Target DateTime: "$NEW_TARGET_DATETIME"
-NEW_TARGET_DATETIME_REF=$( echo $(date +'%Y-%m-%dT%H:%M:%SZ' --date "$CURRENT_DATETIME +$NEW_TARGET days") )
-echo New Target DateTime Ref: "$NEW_TARGET_DATETIME_REF"
+if [[ $MILESTONE_LINKED == "true" ]];
+then
+    echo A milestone is already linked!
+    echo New milestone cannot be created and linked
+else
+    echo There is no milestone linked to the pull request
 
-# Get all OPEN milestones with a due date newer than the current date and time
-MILESTONE_DATA=$( curl --silent -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"${REPOSITORY}"/milestones?state=open\&sort=due_on\&direction=asc )
-FUTURE_MILESTONES=$( echo $MILESTONE_DATA | jq --raw-output '.[] | select(.due_on >= '\"$CURRENT_DATETIME\"')' )
+    CURRENT_DATETIME=$( echo $(date +'%Y-%m-%dT%H:%M:%SZ') )
+    echo Current DateTime: "$CURRENT_DATETIME"
+    LOWER_BOUND_DATETIME_REF=$( echo $(date +'%Y-%m-%dT%H:%M:%SZ' --date "$CURRENT_DATETIME +$LOWER_BOUND days") )
+    echo Lower Bound DateTime Ref: "$LOWER_BOUND_DATETIME_REF"
+    UPPER_BOUND_DATETIME_REF=$( echo $(date +'%Y-%m-%dT%H:%M:%SZ' --date "$CURRENT_DATETIME +$UPPER_BOUND days") )
+    echo Upper Bound DateTime Ref: "$UPPER_BOUND_DATETIME_REF"
+    NEW_TARGET_DATETIME_REF=$( echo $(date +'%Y-%m-%dT%H:%M:%SZ' --date "$CURRENT_DATETIME +$NEW_TARGET days") )
+    echo New Target DateTime Ref: "$NEW_TARGET_DATETIME_REF"
 
-echo Future Milestones:
-echo "$FUTURE_MILESTONES"
+    # Get all OPEN milestones with a due date newer than the current date and time
+    MILESTONE_DATA=$( curl --silent -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"${REPOSITORY}"/milestones?state=open\&sort=due_on\&direction=asc )
+    FUTURE_MILESTONES=$( echo $MILESTONE_DATA | jq --raw-output '.[] | select((.due_on >= '\"$LOWER_BOUND_DATETIME_REF\"') and (.due_on <= '\"$UPPER_BOUND_DATETIME_REF\"'))' )
 
+    echo Future Milestones:
+    echo "$FUTURE_MILESTONES"
 
+    if [[ $FUTURE_MILESTONES != '' ]];
+    then
+        echo Future Milestone List is NOT EMPTY
 
-# STEPS
-# Check for linked milestone
-# Retrieve open milestones that are in the future (ie. due on datetime >= current datetime)
-# Filter results where due on date is >= current datetime + lower bound days
-# Take the milestone number of the first milestone in the list
-# If no future milestone exists then create a new one
-# Take the milestone number of this newly created milestone
-# Assign milestone to pull request
+        MILESTONE=$( echo $FUTURE_MILESTONES | jq --raw-output '.[0]' )
 
-#NEW_MILESTONE=$( curl --silent -X POST -H "Authorization: token ${SECRETS_TOKEN}" "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"${REPOSITORY}"/milestones -d '{"title":"title","due_on":""}' )
-#UPDATE_PULL_REQUEST=$( curl -X PATCH -H "Authorization: token ${SECRETS_TOKEN}" "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"${REPOSITORY}"/issues/"${PULL_REQUEST_NUMBER}" -d '{"milestone":"1"}' )
+        echo Milestone:
+        echo "$MILESTONE"
+
+        echo Milestone Number:
+        MILESTONE_NUMBER=$( $MILESTONE | jq --raw-output '.number' )
+        echo "$MILESTONE_NUMBER"
+
+        link_milestone_curl_cmd $MILESTONE_NUMBER
+        echo Linked Pull Request:
+        echo "$LINKED_PULL_REQUEST"
+    else
+        echo Future Milestone List is EMPTY
+
+        create_milestone_curl_cmd $NEW_TARGET_DATETIME_REF $NEW_TARGET_DATETIME_REF
+
+        echo Created Milestone:
+        echo "$CREATED_MILESTONE"
+
+        MILESTONE_NUMBER=$( $CREATED_MILESTONE | jq --raw-output '.number' )
+        link_milestone_curl_cmd $MILESTONE_NUMBER
+
+        echo Linked Pull Request:
+        echo "$LINKED_PULL_REQUEST"
+    fi
+fi
